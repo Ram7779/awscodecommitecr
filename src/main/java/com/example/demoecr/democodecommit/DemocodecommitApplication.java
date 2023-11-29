6d5aff35a3b52064c6e607d713d9553d47547d9e
ram7779 <yuvaram9@gmail.com>
2023-11-29 14:10:16 +0000

    first commit

---
+++ src/main/java/com/example/demoecr/democodecommit/DemocodecommitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoecr.democodecommit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocodecommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocodecommitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoecr/democodecommit/DemocodecommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoecr.democodecommit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocodecommitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
